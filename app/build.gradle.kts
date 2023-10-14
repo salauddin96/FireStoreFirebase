afec7c1b185624c9b130e20c471e0b8130c4e920
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-10-14 12:39:12 +0600

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ android {
         viewBinding = true
     }
     namespace = "org.techtales.firestoredatabase"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.techtales.firestoredatabase"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/org/techtales/firestoredatabase/MainActivity.kt
+++ app/src/main/java/org/techtales/firestoredatabase/MainActivity.kt
@@ -2,6 +2,7 @@ package org.techtales.firestoredatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import org.techtales.firestoredatabase.databinding.ActivityMainBinding
@@ -18,15 +19,15 @@ class MainActivity : AppCompatActivity(), DataAdapter.ItemClickListener {
         setContentView(binding.root)
 
         adapter = DataAdapter(data, this)
-        binding.recyclerView.adapter=adapter
+        binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
-        binding.addBtn.setOnClickListener{
+        binding.addBtn.setOnClickListener {
             val title = binding.enterEtxt.text.toString()
             val description = binding.enterEdes.text.toString()
 
-            if (title.isNotEmpty() && description.isNotEmpty()){
-                addData(title,description)
+            if (title.isNotEmpty() && description.isNotEmpty()) {
+                addData(title, description)
             }
         }
         fetchData()
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity(), DataAdapter.ItemClickListener {
         dataCollection.get()
             .addOnSuccessListener {
                 data.clear()
-                for (document in it){
+                for (document in it) {
                     val item = document.toObject(Data::class.java)
                     item.id = document.id
                     data.add(item)
@@ -46,25 +47,60 @@ class MainActivity : AppCompatActivity(), DataAdapter.ItemClickListener {
     }
 
     private fun addData(title: String, description: String) {
-        val newData = Data(title=title, description = description)
+        val newData = Data(title = title, description = description)
         dataCollection.add(newData)
             .addOnSuccessListener {
                 newData.id = it.id
                 data.add(newData)
                 adapter.notifyDataSetChanged()
+                Toast.makeText(this, "Data added Successfully", Toast.LENGTH_SHORT).show()
 
                 binding.enterEtxt.text?.clear()
                 binding.enterEdes.text?.clear()
             }
+            .addOnFailureListener {
+                Toast.makeText(this, "Data added Failed", Toast.LENGTH_SHORT).show()
+            }
     }
 
     override fun onEditClick(data: Data) {
-        TODO("Not yet implemented")
+        binding.enterEtxt.setText(data.title)
+        binding.enterEtxt.setText(data.description)
+        binding.addBtn.text = "Update"
+
+        binding.addBtn.setOnClickListener {
+            val updateTitle = binding.enterEtxt.text.toString()
+            val updateDescription = binding.enterEdes.text.toString()
+
+            if (updateTitle.isNotEmpty() && updateDescription.isNotEmpty()) {
+                val updateData = Data(data.id, updateTitle, updateDescription)
+
+                dataCollection.document(data.id!!)
+                    .set(updateData)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Data Updated Failed", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
     }
 
-    override fun onDeleteItemClick(data: Data) {
-        TODO("Not yet implemented")
+
+        override fun onDeleteItemClick(data: Data) {
+            dataCollection.document(data.id!!)
+                .delete()
+                .addOnSuccessListener {
+                    adapter.notifyDataSetChanged()
+                    Toast.makeText(this, "Data deleted", Toast.LENGTH_SHORT).show()
+                    fetchData()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Data deleted failed", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+
+
     }
-
-
-}
