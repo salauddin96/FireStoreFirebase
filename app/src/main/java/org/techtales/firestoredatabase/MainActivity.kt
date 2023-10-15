8cfdbad8d2ab8b126c254faeffa3dd0cc55dfeb2
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-10-15 13:01:22 +0600

    update activity

--- app/src/main/java/org/techtales/firestoredatabase/MainActivity.kt
+++ app/src/main/java/org/techtales/firestoredatabase/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.techtales.firestoredatabase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -42,7 +43,11 @@ class MainActivity : AppCompatActivity(), DataAdapter.ItemClickListener {
                     item.id = document.id
                     data.add(item)
                 }
+                adapter.notifyDataSetChanged()
 
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Data fetch failed", Toast.LENGTH_SHORT).show()
             }
     }
 
@@ -78,6 +83,9 @@ class MainActivity : AppCompatActivity(), DataAdapter.ItemClickListener {
                 dataCollection.document(data.id!!)
                     .set(updateData)
                     .addOnSuccessListener {
+                        binding.enterEtxt.text?.clear()
+                        binding.enterEdes.text?.clear()
+                        startActivity(Intent(this@MainActivity, MainActivity::class.java))
                         Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show()
                     }
                     .addOnFailureListener {
